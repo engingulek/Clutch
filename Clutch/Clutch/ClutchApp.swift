236215f14673603443699f7111eacbbfa7779b94
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2025-10-14 17:13:37 +0300

    added ClutchManagerKits
    added ClutchManagerKits for Page manager.

--- Clutch/Clutch/ClutchApp.swift
+++ Clutch/Clutch/ClutchApp.swift
@@ -12,10 +12,14 @@ import ClutchModularProtocols
 import ClutchOnboardingModule
 import ClutchAccountModule
 import ClutchSelectFavTeamModule
+import ClutchManagerKits
+
 
 @main
 struct ClutchApp: App {
     @StateObject private var navigation = Navigation()
+    private var pageManager : PageManagerProtocol = PageManager()
+    private var firstPage : Page = .onboarding
     init() {
         let container = DependencyRegister.shared.container
         container.register(OnboardingModuleProtocol.self) { resolver in
@@ -29,12 +33,22 @@ struct ClutchApp: App {
         container.register(SelectFavoriteTeamModuleProtocol.self) { resolver in
             ClutchSelectFavTeamModule()
         }
+        
+        
+        switch pageManager.currentState {
+        case .onboarding:
+            print("Page onboarding")
+            firstPage = .onboarding
+        case .accountPage:
+            print("Page account")
+            firstPage = .account
+        }
     }
     
     var body: some Scene {
         WindowGroup {
             NavigationStack(path: $navigation.path) {
-                navigation.build(page: .onboarding)
+                navigation.build(page: firstPage)
                     .navigationDestination(for: Page.self) { page in
                         navigation.build(page: page)
                     }
